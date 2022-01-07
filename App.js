5aa802a5ddaef85ee4161e41c9e4015634588a32
Ashish Malla <ashishmalla45@gmail.com>
2022-01-07 00:57:29 -0700

    Test Image element

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const handlePress = () => console.log('helllllloooo')
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text numberOfLines={1} onPress={handlePress}>
+				Welcome Asiisii!!
+			</Text>
+			{/* <Image style={styles.tinyLogo} source={require('@expo/snack-static/react-native-logo.png')} /> */}
+			<Image
+				blurRadius={4}
+				source={{
+					width: 200,
+					height: 300,
+					uri: 'https://picsum.photos/200/300',
+				}}
+			/>
+			<StatusBar style='auto' />
+		</SafeAreaView>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	tinyLogo: {
+		width: 50,
+		height: 50,
+	},
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
